290710c594b4f6a04d8ec382edc9ae1c52d80c13
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-05-02 22:45:01 +0200

    Implement accelerometer

--- src/app/sensors/accelerometer/accelerometer.component.ts
+++ src/app/sensors/accelerometer/accelerometer.component.ts
@@ -1,10 +1,20 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core'
+import {AccelerometerService} from "~/app/accelerometer.service";
+import {Observable} from "rxjs";
+import {AccelerometerData} from "nativescript-accelerometer";
 
 @Component({
   selector: 'ns-items',
   templateUrl: './accelerometer.component.html',
 })
 export class AccelerometerComponent implements OnInit {
+  public data$: Observable<AccelerometerData>;
+
+  constructor(public accService: AccelerometerService) {
+    this.data$ = accService.data$;
+  }
+
   ngOnInit(): void {
+    this.accService.start();
   }
 }
